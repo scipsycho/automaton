239a7cd6d481915662d8fa0175c31c1eaac98055
Dharmesh Singh <dsingh3@atlassian.com>
2020-10-28 19:46:52 +0530

    removed unneccessary eventlistener

--- public/scripts/background/main.js
+++ public/scripts/background/main.js
@@ -1,6 +1,2 @@
-chrome.browserAction.onClicked.addListener(tab => {
-        chrome.tabs.sendMessage(tab.id, {ask_url: true}, function (response) {
-            alert(response.url)
-        })
-})
+
 
